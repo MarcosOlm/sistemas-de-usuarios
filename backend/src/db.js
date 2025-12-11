6d8da0d3698211f7d450776fb95475e4e2cf124f
unknown <marcoslimado12384@gmail.com>
2025-12-11 16:06:32 -0300

    Backend para o sistemas de ususários

---
+++ backend/src/db.js
@@ -0,0 +1,128 @@
+require('dotenv').config();
+const mysql = require("mysql2/promise");
+
+// connection with MySql
+let connection;
+
+console.log("ENV FROM DB.JS:", {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASSWORD,
+  db: process.env.DB_NAME
+});
+
+async function connect() {
+  if (!connection) {
+    connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+  }
+  return connection;
+}
+
+async function getUsers() {
+  try {
+    const conn = await connect();
+    const [rows] = await conn.query(
+      "SELECT name, email, telephone FROM usuarios"
+    );
+    return {
+      status: true,
+      message: "usuários encontrados",
+      users: rows,
+    };
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function getUserById(idUser) {
+  try {
+    const conn = await connect();
+    const [row] = await conn.query(
+      "SELECT name, email, telephone FROM usuarios WHERE idUser = ?",
+      [idUser]
+    );
+    return {
+      status: true,
+      message: "usuário encontrado",
+      user: row,
+    };
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function createUser(name, email, password) {
+  try {
+    const conn = await connect();
+    await conn.query(
+      "INSERT INTO usuarios (name, email, password) VALUES (?, ?, ?)",
+      [name, email, password]
+    );
+    return {
+      status: true,
+      message: "usuário cadastrado com sucesso",
+    };
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function deleteUser(idUser) {
+  try {
+    const conn = await connect();
+    await conn.query("DELETE FROM usuarios WHERE idUser = ?", [idUser]);
+    return {
+      status: true,
+      message: "usuário deletado com sucesso",
+    };
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function updateUser(field) {
+  // {
+  //   idUser: 1,
+  //   name: 'marcos',
+  //   email: 'marcos@email.com',
+  //   telephone: "83900000000",
+  // };
+
+  try {
+    const conn = await connect();
+    await conn.query(
+      `UPDATE usuarios SET ${checkField(field)} WHERE idUser = ${field.idUser}`
+    );
+    return {
+      status: true,
+      message: "dados do usuário alterados com sucesso",
+    };
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Recursive function
+
+function checkField(field, index=0, query=[]) {
+  if (Object.entries(field).length == index) {
+    return query
+  }
+  if (Object.keys(field).at(index) != 'idUser') {
+    query.push(`${Object.keys(field).at(index)} = '${Object.values(field).at(index)}'`)
+  }
+  return checkField(field, index+1, query);
+}
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  deleteUser,
+  updateUser,
+};
---
+++ backend/src/server.js
@@ -0,0 +1,59 @@
+const port = 5000;
+require('dotenv').config();
+const cors = require('cors')
+const express = require('express')
+const app = express()
+const db = require('./db.js')
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/user', async (req, res) => {
+    try {
+        res.send(await db.getUsers());
+    }
+    catch (err) {
+        console.log(err);
+    }
+})
+
+app.get('/user/:id', async (req, res) => {
+    try {
+        res.send(await db.getUserById(req.idUser));
+    }
+    catch (err) {
+        console.log(err);
+    }
+})
+
+app.post('/user', async (req, res) => {
+    try {
+        const {name, email, password} = req.body
+        res.send(await db.createUser(name, email, password));
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
+
+app.delete('/user/:id', async (req, res) => {
+    try {
+        res.send(await db.deleteUser(req.idUser));
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
+
+app.put('/user', async (req, res) => {
+    try {
+        res.send(await db.updateUser(req.body));
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server on: ${port}`)
+})
