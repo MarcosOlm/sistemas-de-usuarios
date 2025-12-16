1ca34d5ada33a82d678ae03fd7f61ff1bb7eded4
unknown <marcoslimado12384@gmail.com>
2025-12-16 16:06:18 -0300

    Criando o service para conectar com o backend

--- frontend/src/services/useService.tsx
+++ frontend/src/services/useService.tsx
@@ -1,18 +1,49 @@
 import api from "./api";
 
-type FormData = {
-  name?: string;
-  email: string;
-  password: string;
+interface FormData {
+  name?: string,
+  email?: string,
+  password?: string,
+  telephone?: string,
 };
 
+interface reqResponse {
+    status: boolean,
+    massage: string,
+}
+
 const userService = {
-    async createUser(data: FormData) {
-        const response = await api.post("/user", data);
-        return response;
-    },    
     async loginUser(data: FormData) {
-        const response = await api.post("/auth/login", data);
+        const {email, password} = data
+        const response = await api.post<reqResponse>("/login", {email, password});
+        return response;
+    },
+    async logout() {
+        const response = await api.post<reqResponse>("/logout");
+        return response;
+    },
+    async createUser(data: FormData) {
+        const response = await api.post<reqResponse>("/registration", data);
+        return response;
+    },
+    async auth() {
+        const response = await api.get<reqResponse>("/me");
+        return response;
+    },
+    async getAllUsers() {
+        const response = await api.get<reqResponse>("/allUser");
+        return response;
+    },
+    async getUser() {
+        const response = await api.get<reqResponse>("/user");
+        return response;
+    },
+    async deleteUser() {
+        const response = await api.delete<reqResponse>("/user");
+        return response;
+    },
+    async updateUser(data: FormData) {
+        const response = await api.put<reqResponse>("/user", data);
         return response;
     }
 }
